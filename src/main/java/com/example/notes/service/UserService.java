bd5a61ac905cf3891bbf7dd2a6b321c01a864fb5
nikita <nik_z12@mail.ru>
2022-11-25 22:35:04 +0300

    work with the service

--- src/main/java/com/example/notes/service/UserService.java
+++ src/main/java/com/example/notes/service/UserService.java
@@ -1,4 +1,44 @@
 package com.example.notes.service;
 
+import com.example.notes.domain.User;
+import com.example.notes.dto.requests.CreateUserRequest;
+import com.example.notes.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
 public class UserService implements UserServiceImp{
+    public static final String USER_NOT_FOUND_MESSAGE = "User Not Found";
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Iterable<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public User createUser(@NotNull CreateUserRequest user) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> update(@NotNull CreateUserRequest request, UUID userId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteUser(UUID userId) {
+
+    }
+
+    @Override
+    public Optional<User> findByUuid(UUID userId) {
+        return Optional.empty();
+    }
 }
--- src/main/java/com/example/notes/service/UserServiceImp.java
+++ src/main/java/com/example/notes/service/UserServiceImp.java
@@ -1,4 +1,16 @@
 package com.example.notes.service;
 
+import com.example.notes.domain.User;
+import com.example.notes.dto.requests.CreateUserRequest;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserServiceImp {
+    Iterable<User> findAll();
+    User createUser(@NotNull CreateUserRequest user);
+    Optional<User> update(@NotNull CreateUserRequest request, UUID userId);
+    void deleteUser(UUID userId);
+    Optional<User> findByUuid(UUID userId);
 }
