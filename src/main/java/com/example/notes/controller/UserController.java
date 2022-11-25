02c7d4e6d25ea0f3b363ccc3c605557b8b6bbfba
nikita <nik_z12@mail.ru>
2022-11-25 22:48:01 +0300

    started developing the controller

---
+++ src/main/java/com/example/notes/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.notes.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+}
